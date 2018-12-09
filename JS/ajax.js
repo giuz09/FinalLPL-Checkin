714f74c78a7958ace32052c83d9dbb4738484873
Unknown <liamoreno14@gmail.com>
2018-12-09 20:25:20 -0300

    Validacion campos vacios

--- JS/ajax.js
+++ JS/ajax.js
@@ -1,4 +1,38 @@
-function obtenerXHR(){
+
+
+function validoCampos(){
+
+	var dni =document.getElementById('dni').value.length;
+	var nombre =document.getElementById('nombre').value.length;
+	var fechaViaje =document.getElementById('fechaViajar').value.length;
+	var nroVuelo =document.getElementById('nroVuelo').value.length;
+	var isOK=true;
+
+	if (dni == "") {
+		isOK=false;
+		alert("¡Atención! Debe ingresar su numero de DNI.");
+	};
+	if (nombre =="") {
+		isOK=false;
+		alert("¡Atención! Falta igresar su nombre y apellido.");
+	};
+	if (fechaViaje == "") {
+		isOK=false;
+		alert("¡Atención! Usted no selecciono ningúna fecha de viaje.");
+	};
+	if (nroVuelo == "") {
+		isOK=false;
+		alert("¡Atención! Falta ingresar el numero de vuelo.");
+	};
+	
+
+	if (isOK) { //procedo a validar que exista ese viaje
+		return isOK;
+	}
+
+	}
+
+	function obtenerXHR(){
 	req = false;
 	if (window.XMLHttpRequest) {
 		req = new XMLHttpRequest();
@@ -10,7 +44,7 @@ function obtenerXHR(){
 	return req;
 }
 
-function desplegarDatosPasajero(){
+	function desplegarDatosPasajero(){
 	peticion = obtenerXHR();	
 	var valor = document.getElementById("dni").value;
 	peticion.open("GET","datosPasajero.php?dni="+valor,true);
@@ -26,5 +60,5 @@ function desplegarDatosPasajero(){
 				document.getElementById("respuesta").innerHTML = peticion.responseText;
 			}
 		}
-	}
-}
+	}	
+}*/
