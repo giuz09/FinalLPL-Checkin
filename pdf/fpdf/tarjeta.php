dd1fae31ea5f4ea135d6c6b6887177a85db76e9b
Unknown <liamoreno14@gmail.com>
2018-12-13 20:15:45 -0300

    datos modificados

--- pdf/fpdf/tarjeta.php
+++ pdf/fpdf/tarjeta.php
@@ -7,10 +7,13 @@ $pdf = new FPDF('P','mm','A4');
 ///////////////////////////
 $pdf->AddPage();
 
-$pdf->SetFont('Arial','B',15);
-$pdf->Cell(190,10,'TARJETA DE EMBARQUE ',1,1,'C');
-$pdf->Cell(95,50,'AEP 22:30-------------------------',1,0,'L');
-$pdf->Cell(95,50,'CDR 00:45-------------------------',1,0,'L');
+$pdf->SetFont('Arial','B',12);
+$pdf->Cell(190,10,'TARJETA DE EMBARQUE ',0,1,'C');
+$pdf->Cell(190,10,'Gonzalez Juan Perez ',1,1,'C');
+$pdf->Cell(150,20,'Origen: AEP - 14/12/2018 - 22:30hs',1,1,'L');
+$pdf->Cell(150,20,'Destino: CDR - 21/12/2018 - 00:45hs',1,1,'L');
+$pdf->Cell(190,8,'Nro. vuelo: 2098 - Asiento: 5B ',1,1,'C');
+$pdf->Cell(190,4,'Recordar presentarse 1:30hr  antes de su vuelo ',1,1,'C');
 /////////////////////////////
 $pdf->Output();
 
